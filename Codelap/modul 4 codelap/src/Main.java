715e14143bee15e4300185c9bed0aa5a81ecc1e0
Lays4u <dragstercmbl@gmail.com>
2025-04-29 19:37:32 +0700

    update isi file

---
+++ Codelap/modul 4 codelap/src/Main.java
@@ -0,0 +1,31 @@
+package app;
+
+import perpustakaan.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Buku nonFiksi = new NonFiksi("Atomic Habit", "James Clear", "Pengembangan Diri");
+        Buku fiksi = new Fiksi("One Piece Vol. 111", "Eiichiro Oda", "Shonen");
+
+        nonFiksi.displayInfo();
+        fiksi.displayInfo();
+
+        System.out.println();
+
+        Anggota anggota1 = new Anggota("Jatmiko Eka Pratama", "C087");
+        Anggota anggota2 = new Anggota("Damar Ghulamul Abror ", "C060");
+
+        anggota1.displayInfo();
+        anggota2.displayInfo();
+
+        System.out.println();
+
+        anggota1.pinjamBuku("Atomic Habit");
+        anggota2.pinjamBuku("One Piece Vol. 111", 7);
+
+        System.out.println();
+
+        anggota1.kembalikanBuku("Atomic Habit");
+        anggota2.kembalikanBuku("One Piece Vol. 111");
+    }
+}
